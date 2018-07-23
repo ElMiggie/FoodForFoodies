405d48f50bd6c4cb464dc25a1fa39058f5091140
Miguel <miguelsiordia99@gmail.com>
2018-07-23 09:07:21 -0700

    testing

--- main.py
+++ main.py
@@ -45,11 +45,17 @@ class FoodHandler(webapp2.RequestHandler):
 class ShowFoodHandler(webapp2.RequestHandler):
     def get(self):
         food_list_template = jinja_current_dir.get_template("templates/foodlist.html")
-        fav_foods = Food.query().order(-Food.food_name).fetch(3)
-        dict_for_template = {'top_fav_foods': fav_foods}
-        self.response.write(food_list_template.render(dict_for_template))
+        # fav_foods = Food.query().order(-Food.food_name).fetch(3)
+        # dict_for_template = {'top_fav_foods': fav_foods}
+        # self.response.write(food_list_template.render(dict_for_template))
+        html = food_list_template.render({
+        'food_calories': self.response.,
+        'food_fats': self.response.,
+        'food_sodium': self.response.,
+        'food_carbs': self.response.,
+        )}
 
 app = webapp2.WSGIApplication([
     ('/', FoodHandler),
-    ('/showfavs', ShowFoodHandler)
+    ('/nutrition', ShowFoodHandler)
 ], debug=True)
