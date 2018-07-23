cc81ee7e65735940911f628d04f9228fbc704431
Ankita Kundu <ankitakundu0727@gmail.com>
2018-07-23 15:37:13 -0400

    Fixed recipe problem

--- main.py
+++ main.py
@@ -43,21 +43,22 @@ class MainHandler(webapp2.RequestHandler):
         'recipe4Name': foods[foodIndex].recipe4Name,
         'recipe5Name': foods[foodIndex].recipe5Name,
         })
-    def post (self):
+    def post(self):
         apple = models.Food(food_name = "Apple", recipe1Name = "Apple Empanadas", recipe2Name = "All-American Apple Pie", recipe3Name = "Danish Apple Cake", recipe4Name = "Apple Slaw", recipe5Name = "South African Apple Tart")
         peaches = models.Food(food_name = "Peaches", recipe1Name = "Postre Chaja Peach Meringue Cake", recipe2Name = "Peach Cobbler", recipe3Name = "Gooey Peach Dumplings", recipe4Name = "Peach Chicken", recipe5Name = "Peach Phrini")
         apple.put()
         peaches.put()
-        apple_pie= models.Recipe(ingredients=["1/3 to 1/2 cup sugar", "1/4 cup Gold Medal all-purpose flour", "1/2 teaspoon ground cinnamon", "1/2 teaspoon ground nutmeg", "1/8 teaspoon salt", "8 cups thinly sliced peeled tart apples (8 medium)", "2 tablespoons butter or margarine"],
-               directions=["Heat oven to 425ºF. Prepare Double-Crust Pastry", "Mix sugar, flour, cinnamon, nutmeg and salt in large bowl. Stir in apples. Turn into pastry-lined pie plate. Dot with butter. Trim overhanging edge of pastry 1/2 inch from rim of plate", "Roll other round of pastry. Fold into fourths and cut slits so steam can escape. Unfold top pastry over filling; trim overhanging edge 1 inch from rim of plate. Fold and roll top edge under lower edge, pressing on rim to seal; flute as desired. Cover edge with 3-inch strip of aluminum foil to prevent excessive browning. Remove foil during last 15 minutes of baking", "Bake 40 to 50 minutes or until crust is brown and juice begins to bubble through slits in crust. Serve warm if desired."]
-        apple_pie.put()
-        )
 
 class RecipeHandler (webapp2.RequestHandler):
     def get (self):
         recipes=jinja_current_dir.get_template("templates/results.html")
         html=recipes.render({})
         self.response.write(html)
+
+    # def post(self):
+    #     apple_pie= models.Recipe(ingredients=["1/3 to 1/2 cup sugar", "1/4 cup Gold Medal all-purpose flour", "1/2 teaspoon ground cinnamon", "1/2 teaspoon ground nutmeg", "1/8 teaspoon salt", "8 cups thinly sliced peeled tart apples (8 medium)", "2 tablespoons butter or margarine"],
+    #            directions=["Heat oven to 425ºF. Prepare Double-Crust Pastry", "Mix sugar, flour, cinnamon, nutmeg and salt in large bowl. Stir in apples. Turn into pastry-lined pie plate. Dot with butter. Trim overhanging edge of pastry 1/2 inch from rim of plate", "Roll other round of pastry. Fold into fourths and cut slits so steam can escape. Unfold top pastry over filling; trim overhanging edge 1 inch from rim of plate. Fold and roll top edge under lower edge, pressing on rim to seal; flute as desired. Cover edge with 3-inch strip of aluminum foil to prevent excessive browning. Remove foil during last 15 minutes of baking", "Bake 40 to 50 minutes or until crust is brown and juice begins to bubble through slits in crust. Serve warm if desired."]
+    #     apple_pie.put()
 # class FoodHandler(webapp2.RequestHandler):
 #     def get(self):
 #         start_template = jinja_current_dir.get_template("templates/welcome.html")
@@ -95,11 +96,6 @@ class InfoHandler(webapp2.RequestHandler):
         'food_sodium': self.response.,
         'food_carbs': self.response.,
         )}
-          recipe_template=jinja_current_dir.get_template("templates/results.html")
-          rendered_recipe=recipe_template.render({
-          # 'recipe_picture': recipe_picture
-          })
-          self.response.write(rendered_recipe)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
