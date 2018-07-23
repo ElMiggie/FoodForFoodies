8b245fbef1d5b11bf5bdb5106e54d9ef91b074c7
Alex Konzmann <akonzmann88@gmail.com>
2018-07-23 12:35:50 -0400

    made the datastore thing work

--- main.py
+++ main.py
@@ -20,7 +20,8 @@ import webapp2
 import random
 import os
 import jinja2
-from models import Food
+import models
+from random_food_data import seed_data
 
 #remember, you can get this by searching for jinja2 google app engine
 jinja_current_dir = jinja2.Environment(
@@ -30,11 +31,31 @@ jinja_current_dir = jinja2.Environment(
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        start_template = jinja_current_dir.get_template("Templates/welcome.html")
-        self.response.write(start_template.render())
-
-        randomFoodList = ["apple", "peaches", "spaghetti"]
+        randomFoodList = ["Apple", "Peaches", "Spaghetti"]
         foodOfDay = random.choice(randomFoodList)
+        foods = models.Food.query().fetch()
+        start_template = jinja_current_dir.get_template("templates/welcome.html")
+        self.response.write(start_template.render({
+        'food_name': foodOfDay,
+        'recipe1Name': foods[0].recipe1Name,
+        'recipe2Name': foods[0].recipe2Name,
+        'recipe3Name': foods[0].recipe3Name,
+        'recipe4Name': foods[0].recipe4Name,
+        'recipe5Name': foods[0].recipe5Name,
+        }))
+
+class RandomFoodHandler(webapp2.RequestHandler):
+    def get(self):
+        apple = models.Food(food_name = "Apple", recipe1Name = "Apple Empanadas", recipe2Name = "All-American Apple Pie", recipe3Name = "Danish Apple Cake", recipe4Name = "Apple Slaw", recipe5Name = "South African Apple Tart")
+        peaches = models.Food(food_name = "Peaches")
+        spaghetti = models.Food(food_name = "Spaghetti")
+
+        apple.put()
+
+
+
+
+
 
 
 # class FoodHandler(webapp2.RequestHandler):
@@ -62,31 +83,32 @@ class MainHandler(webapp2.RequestHandler):
 #         self.response.write(food_list_template.render(dict_for_template))
 
 
-class RecipeHandler(webapp2.RequestHandler):
-    def get(self):
-        food_list_template = jinja_current_dir.get_template("templates/foodlist.html")
-        # fav_foods = Food.query().order(-Food.food_name).fetch(3)
-        # dict_for_template = {'top_fav_foods': fav_foods}
-        # self.response.write(food_list_template.render(dict_for_template))
-        html = food_list_template.render({
-        'food_calories': self.response.,
-        'food_fats': self.response.,
-        'food_sodium': self.response.,
-        'food_carbs': self.response.,
-        )}
-
-app = webapp2.WSGIApplication([
-    ('/', FoodHandler),
-    ('/nutrition', ShowFoodHandler)
-
-          recipe_template=jinja_current_dir.get_template("templates/results.html")
-          rendered_recipe=recipe_template.render({
-          # 'recipe_picture': recipe_picture
-          })
-          self.response.write(rendered_recipe)
+# class RecipeHandler(webapp2.RequestHandler):
+#     def get(self):
+#         food_list_template = jinja_current_dir.get_template("templates/foodlist.html")
+#         # fav_foods = Food.query().order(-Food.food_name).fetch(3)
+#         # dict_for_template = {'top_fav_foods': fav_foods}
+#         # self.response.write(food_list_template.render(dict_for_template))
+#         html = food_list_template.render({
+#         'food_calories': self.response.,
+#         'food_fats': self.response.,
+#         'food_sodium': self.response.,
+#         'food_carbs': self.response.,
+#         )}
+#
+# app = webapp2.WSGIApplication([
+#     ('/', FoodHandler),
+#     ('/nutrition', ShowFoodHandler)
+#
+#           recipe_template=jinja_current_dir.get_template("templates/results.html")
+#           rendered_recipe=recipe_template.render({
+#           # 'recipe_picture': recipe_picture
+#           })
+#           self.response.write(rendered_recipe)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
+    ('/random', RandomFoodHandler),
     # ('/', FoodHandler),
     # ('/showfavs', ShowFoodHandler),
     # ('/recipes', RecipeHandler)
--- random_food_data.py
+++ random_food_data.py
@@ -4,3 +4,5 @@ def seed_data():
     apple = Food(food_name = "Apple", recipe1Name = "Apple Empanadas", recipe2Name = "All-American Apple Pie", recipe3Name = "Danish Apple Cake", recipe4Name = "Apple Slaw", recipe5Name = "South African Apple Tart")
     peaches = Food(food_name = "Peaches")
     spaghetti = Food(food_name = "Spaghetti")
+
+    apple.put()
