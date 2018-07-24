a57155fda270cc53753632568cbe512780decdc7
Ankita Kundu <ankitakundu0727@gmail.com>
2018-07-24 16:45:33 -0400

    Pushing the image

--- main.py
+++ main.py
@@ -63,11 +63,7 @@ class RandomFoodHandler(webapp2.RequestHandler):
 
 class RecipeEntryHandler(webapp2.RequestHandler):
     def get(self):
-        directions_array=["Heat oven to 425F. Prepare Double-Crust Pastry", "Mix sugar, flour, cinnamon, nutmeg and salt in large bowl. Stir in apples. Turn into pastry-lined pie plate. Dot with butter. Trim overhanging edge of pastry 1/2 inch from rim of plate", "Roll other round of pastry. Fold into fourths and cut slits so steam can escape. Unfold top pastry over filling; trim overhanging edge 1 inch from rim of plate. Fold and roll top edge under lower edge, pressing on rim to seal; flute as desired. Cover edge with 3-inch strip of aluminum foil to prevent excessive browning. Remove foil during last 15 minutes of baking", "Bake 40 to 50 minutes or until crust is brown and juice begins to bubble through slits in crust. Serve warm if desired."]
-        ingredients_array=["1/3 to 1/2 cup sugar", "1/4 cup Gold Medal all-purpose flour", "1/2 teaspoon ground cinnamon", "1/2 teaspoon ground nutmeg", "1/8 teaspoon salt", "8 cups thinly sliced peeled tart apples (8 medium)", "2 tablespoons butter or margarine"]
-        apple_pie= models.Recipe(food_name="apple_pie", ingredients=ingredients_array,
-        directions=directions_array)
-        apple_pie.put()
+
 
 class RecipeHandler (webapp2.RequestHandler):
     def get (self):
@@ -100,6 +96,7 @@ class InfoHandler(webapp2.RequestHandler):
         #person_query = model.Facebook.query().filter(model.Nutrition.name == 'raw_input()')
         #all_food = person_query.fetch()
         ###info = models.Nutrition.query().fetch()
+
         html = food_list_template.render({
         "search_food": self.request.get("search_food"),
         'food_name': apple_pie.name,
